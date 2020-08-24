ba99d9fbb5e0f6d4554f02f735eea26d7aefa044
Daniel Calise <daniel.calise@aexp.com>
2020-08-24 16:21:21 -0400

    adding tests

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import React, { Component } from "react";
-import "./App.css";
+import React from "react";
+import DefaultComponent from './DefaultExport';
+import { FirstNamedComponent, SecondNamedComponent } from './NamedExports';
+
+function DefaultComponent() {
+  return (
+    <div>Default 1</div>
+  );
+}
+
+function FirstNamedComponent() {
+  return (
+    <div>Named Component 1</div>
+  );
+}
+
+function SecondNamedComponent() {
+  return (
+    <div>Named Component 2</div>
+  );
+}
 
 const App = () => {
-  return (<div></div>);
+  return (<div>
+    <DefaultComponent />
+    <FirstNamedComponent />
+    <SecondNamedComponent />
+  </div>);
 };
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,42 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
-import {
-  Grocery,
-  Fridge,
-  ShoppingCart,
-} from './App';
+import DefaultExport from './DefaultExport';
+import App from 'App';
+import { FirstNamedComponent, SecondNamedComponent } from "./NamedExports";
 
 /**************************************************************************************************
  * Volunteers: add you tests here. They should be failing, but the students should be able to make
  * them pass by editing the other files.
  **************************************************************************************************/
 
+describe("The App", () => {
+  test('Should render the 3 expected components', () => {
+    const { getByText } = render(
+      <div>
+        <App />
+      </div>
+    );
 
-describe("Tests", () => {
+    expect(getByText(/Default 1/i).textContent).toBe('Default 1');
+    expect(getByText(/Named Component 1/i).textContent).toBe('Named Component 1');
+    expect(getByText(/Named Component 2/i).textContent).toBe('Named Component 2');
+  });
+});
+
+describe("The components", () => {
+  test('should render as expected', () => {
+    const { getByText } = render(
+      <div>
+        <DefaultExport />
+        <FirstNamedComponent />
+        <SecondNamedComponent />
+      </div>
+    );
+
+    expect(getByText(/Default 1/i).textContent).toBe('Default 1');
+    expect(getByText(/Named Component 1/i).textContent).toBe('Named Component 1');
+    expect(getByText(/Named Component 2/i).textContent).toBe('Named Component 2');
+  });
 });
 
